9d111358447cdf1d65de2ce4bfa029f97c527cd0
segejepe <64331220+segejepe@users.noreply.github.com>
2023-07-13 12:48:33 -0700

    Update3 index.js
    Completed Gmax Parts

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ class GmpHeadparts extends Part { }
 class GmpHandpieceparts extends Part { }
 class GmpControllerparts extends Part { }
 class GmpSkinParts extends Part { }
+class GmpCalportParts extends Part { }
+class GmpServiceTool extends Part { s}
 
 // Gmax pro Plus
 class GmppHandpieceparts extends Part { }
@@ -64,8 +66,10 @@ const opticsParts = [
   new GmpOpticsparts('7122-00-9575', 'DHP FIBER RECEPTACLE', '', 'https://i.ibb.co/58kTRpT/7122-00-9575.jpg', 'F15.1'),
   new GmpOpticsparts('8050-00-9003', 'Fiber foucs lens', 'Fiber foucs lens only', '', ''),
   new GmpOpticsparts('7122-00-9457', 'ASSY,HEAD DETECTOR, GMAX PRO', '', 'https://i.ibb.co/tBSSG5C/7122-00-9457.jpg', ''),
-  new GmpOpticsparts('7122-00-9529', 'BEAM BLOCKER ASSY GMAX PRO', 'Shutter(GMAX PRO)', '', ''),
-  new GmpOpticsparts('7122-00-9410', 'BEAM BLOCKER ASSY', 'For Old system only', '', '')
+  new GmpOpticsparts('7122-00-9529', 'BEAM BLOCKER ASSY GMAX PRO', 'Shutter(GMAX PRO)', '', 'F3.1'),
+  new GmpOpticsparts('7122-00-9410', 'BEAM BLOCKER ASSY', 'For Old system only', '', 'F3.1'),
+  new GmpOpticsparts('7123-00-0202', 'Assy LC DHP Fiber Bundle Kit', '', '', 'Upgrade to Split IO'),
+  new GmpOpticsparts('8075-00-1018', 'Head Detector Fiber (Orange)', '5 Digits IO', '', 'Upgrade to Split IO'),
 ];
 
 // GmpConsumableparts 객체 생성
@@ -77,7 +81,6 @@ const consumableParts = [
   new GmpConsumableparts('7122-00-9561', '3MM SLIDER ATTACHMENT ASSY', '', 'https://i.ibb.co/D5KbY9b/7122-00-9561.jpg', 'F15.1'),
   new GmpConsumableparts('7122-00-9753', 'Assy, 5mm Spot Slider Attachment', '5mm', 'https://i.ibb.co/vLCFDCh/7122-00-9753.jpg', 'F15.1'),
   new GmpConsumableparts('7122-00-9747', 'LARGE SPOT SLIDER ATTACHMENT ASSY', '20/22/24', 'https://i.ibb.co/JxmR7fq/7122-00-9747-1.jpg', 'F15.1'),
-  new GmpConsumableparts('1301-00-9430', 'WINDOW, SPECTRUM CALPORT', 'Calport window', 'https://i.ibb.co/Xkq0k1b/1301-00-9430.jpg', 'F15.1'),
   new GmpConsumableparts('8901-00-9442', 'Preventative Maintenance Kit, Pro', 'PM kit', '', ''),
   new GmpConsumableparts('7122-00-9435', 'SWING ARM,FIBER POLE,ASSY', '', 'https://i.ibb.co/SwGr1rp/7122-00-9435.jpg', 'Only Fiber pole'),
   new GmpConsumableparts('7122-00-9436', 'GUIDE TUBE,FIBER POLE,ASSY,SPECTRUM', '', 'https://i.ibb.co/w0HrRSD/7122-00-9436-1.jpg', ''),
@@ -106,8 +109,6 @@ const consumableParts = [
   new GmpConsumableparts('4001-00-2450', 'Pwr Sply,90-264VIN,15VDC,1A', 'Cryogen Warmer DC adapter', '', ''),
   new GmpConsumableparts('8901-00-9410', 'GentleYAG PRO U conversion to GMAX PRO using Option Kit', '', '', ''),
   new GmpConsumableparts('8901-00-9412', 'GentleLASE PRO U conversion to GMAX PRO using Option Kit', '', '', ''),
-  new GmpConsumableparts('7122-00-3363', 'ASSY,TOOL,ALIGN,CROSSHAIR,MGL', '', 'https://i.ibb.co/FsyjdSt/7122-00-3363.jpg', ''),
-  new GmpConsumableparts('7122-00-3362', 'ASSY,ALIGN VER TOOL,MGL', '', 'https://i.ibb.co/YhBvzVX/7122-00-3362.jpg', ''),
   new GmpConsumableparts('1303-00-0114', 'GAUGE, DISTANCE, 20MM LARGE SPOT', '', 'https://i.ibb.co/tqvXgyn/1303-00-0114.jpg', ''),
   new GmpConsumableparts('7122-00-9758', 'Large Spot Distance Gauge Kit', '', '', ''),
   new GmpConsumableparts('7122-00-9521', 'GL PRO 12mm DIST GAUGE KIT, ASSY', 'PRO Series 12mm Distance Gauge -5Pk', 'https://i.ibb.co/dkD2bLJ/7122-00-9521.jpg', ''),
@@ -132,34 +133,30 @@ const consumableParts = [
 // GmpElectronicsparts 객체 생성
 const electronicsParts = [
   new GmpElectronicsparts('7122-00-9216', 'SPECTRUM AC MODULE', 'Whole Assy from Power cable to AC Distribution Board and relay including cables.', 'https://i.ibb.co/mFdYbF6/7122-00-9216.jpg', 'Power off'),
-  new GmpElectronicsparts('4816-01-1620', '1800V 270A DIODE MODULE', '', 'https://i.ibb.co/7VjLgMT/4816-01-1620.jpg', ''),
-  new GmpElectronicsparts('4001-01-0091', 'HVPS', '', '', ''),
-  new GmpElectronicsparts('7122-00-9608', 'ASSY,GMAX PRO CALPORT', '', '', ''),
-  new GmpElectronicsparts('7122-00-9525', '4 PIN SWITCH ASSY', 'Inside calport', '', ''),
-  new GmpElectronicsparts('7122-00-9524', '5 PIN SWITCH ASSY', 'Inside calport', '', ''),
+  new GmpElectronicsparts('4816-01-1620', '1800V 270A DIODE MODULE', '', 'https://i.ibb.co/7VjLgMT/4816-01-1620.jpg', 'F19.3'),
+  new GmpElectronicsparts('4001-01-0091', 'HVPS', 'High Voltage Power Supply', '', 'F5.1,F5.2,F4.2, F14.1, F14.2'),
+  new GmpElectronicsparts('4001-67-0091', 'HVPS', 'FSE Part', '', 'F5.1,F5.2,F4.2, F14.1, F14.2'),
   new GmpElectronicsparts('7122-00-9597', 'Assy, Harness 1-wire Gmax Pros', '', '', ''),
   new GmpElectronicsparts('8901-00-9404', 'GL PRO FIELD REPLACEMENT DISPLAY ASSY', 'Display for 4 Digits SN (Old)', '', ''),
   new GmpElectronicsparts('7122-00-9596', 'ASSY SIGNAL HARNESS GMAX PRO', '4 Digits SN Part', 'https://i.ibb.co/10M5cKg/7122-00-9596.jpg', ''),
   new GmpElectronicsparts('7111-07-2793', 'AC DIST ROHS PCB ASSY SPECTRUM', '', '', ''),
   new GmpElectronicsparts('7122-00-3330', 'Assy,Linecord,MGL', '', 'https://i.ibb.co/xDqZQH3/7122-00-3330.jpg', ''),
-  new GmpElectronicsparts('7122-00-9235', 'ASSY DCD MODULE SPECTRUM', '', 'https://i.ibb.co/BZDVkjJ/7122-00-9235.jpg', ''),
+  new GmpElectronicsparts('7122-00-9235', 'ASSY DCD MODULE SPECTRUM', '', 'https://i.ibb.co/BZDVkjJ/7122-00-9235.jpg', 'F8.2, F8.4, F9.2'),
   new GmpElectronicsparts('7111-07-2520', 'Assy,BOM,RoHS PCB,DCD Canister', '', 'https://i.ibb.co/1Ts66Zq/7111-07-2520.jpg', ''),
   new GmpElectronicsparts('3409-02-0200', 'FERRULE,SET,1/8T,NYLON', '', 'https://i.ibb.co/VmnJ7SC/3409-02-0200.jpg', ''),
   new GmpElectronicsparts('1301-00-8462', 'CLIP SPR DCD', '', 'https://i.ibb.co/jW4H8MZ/1301-00-8462.jpg', ''),
   new GmpElectronicsparts('3412-26-0220', 'CONN,QC,RCPT,BR,BLKHD,1/8T', '', '', ''),
-  new GmpElectronicsparts('7111-20-2803', 'PRGRM PRO 1-WIRE ALEX HEAD PCB', '', '', ''),
-  new GmpElectronicsparts('7111-21-2803', 'PRGRM PRO 1-WIRE YAG HEAD PCB', '', '', ''),
-  new GmpElectronicsparts('7111-23-2803', 'PRGRM UNIVERSAL LIN PCB', 'LIN PCB', '', ''),
-  new GmpElectronicsparts('7122-00-9597', 'Assy, Harness 1-wire Gmax Pros', '', '', ''),
-  new GmpElectronicsparts('4820-00-0081', 'IGBT MODULE, 1.7kV, 1200A ICRM @ 3ms', '', '', ''),
-  new GmpElectronicsparts('7111-07-2510', 'ASSY, PCB, HP BULKHEAD ROHS', '', '', ''),
+  new GmpElectronicsparts('7111-20-2803', 'PRGRM PRO 1-WIRE ALEX HEAD PCB', '', '', 'F26.1, F23'),
+  new GmpElectronicsparts('7111-21-2803', 'PRGRM PRO 1-WIRE YAG HEAD PCB', '', '', 'F26.1, F23'),
+  new GmpElectronicsparts('7111-23-2803', 'PRGRM UNIVERSAL LIN PCB', 'LIN PCB', '', 'F26.1, F23'),
+  new GmpElectronicsparts('7122-00-9597', 'Assy, Harness 1-wire Gmax Pros', '', '', 'F26.1, F23'),
+  new GmpElectronicsparts('4820-00-0081', 'IGBT MODULE, 1.7kV, 1200A ICRM @ 3ms', '', '', 'F19.1'),
+  new GmpElectronicsparts('7111-07-2510', 'ASSY, PCB, HP BULKHEAD ROHS', '', '', 'W10.2'),
   new GmpElectronicsparts('1510-00-7225', 'CAP 9000UF,1.4V,1125/7875 RND', '', '', ''),
   new GmpElectronicsparts('7111-07-2793', 'AC DIST ROHS PCB ASSY SPECTRUM', '', '', ''),
   new GmpElectronicsparts('8075-00-1014', 'Head Detector Fiber (Orange)', '4 Digits IO', '', ''),
-  new GmpElectronicsparts('8075-00-1015', 'Calport Detector Fiber (Black)', '4 Digits IO', '', ''),
   new GmpElectronicsparts('8075-00-1018', 'Head Detector Fiber (Orange)', '5 Digits IO', '', ''),
-  new GmpElectronicsparts('8075-00-1019', 'Calport Detector Fiber (Black)', '5 Digits IO', '', ''),
-  new GmpElectronicsparts('7111-07-2807', 'ASSY, ROHS, HV DUMP ASSY, GMAX PRO', '', 'https://i.ibb.co/MVL209R/7111-07-2807.jpg', ''),
+  new GmpElectronicsparts('7111-07-2807', 'ASSY, ROHS, HV DUMP ASSY, GMAX PRO', '', 'https://i.ibb.co/MVL209R/7111-07-2807.jpg', 'F5.3, F19.5'),
   new GmpElectronicsparts('1510-00-7225', 'CAP 9000UF,1.4KV,1125/7875 RND', '', '', ''),
   new GmpElectronicsparts('7122-00-3803', 'ASSY,THYR,VBEAM2', '', 'https://i.ibb.co/P4q3jjS/7122-00-3803.jpg', 'F5.2'),
   new GmpElectronicsparts('7122-00-3329', 'Assy,Axial Fan,80MM SQ,28.2CFM', '', 'https://i.ibb.co/7Ghgb0X/7122-00-3329.jpg', '')
@@ -168,12 +165,12 @@ const electronicsParts = [
 
 // GmpFluidsystem 객체 생성
 const fluidSystem = [
-  new GmpFluidsystem('7122-00-9238', 'ASSY RESERVOIR SPECTRUM', 'water tank', 'https://i.ibb.co/P4cb9g8/7122-00-9238.jpg', 'water leaking'),
+  new GmpFluidsystem('7122-00-9238', 'ASSY RESERVOIR SPECTRUM', 'water tank', 'https://i.ibb.co/P4cb9g8/7122-00-9238.jpg', 'water leaking, F7.5'),
   new GmpFluidsystem('3460-00-0071', 'CAP VENT 2 1/4 BUTTRESS THRD', 'cap', 'https://i.ibb.co/rs4d6Nv/3460-00-0071.jpg', ''),
-  new GmpFluidsystem('1301-00-9390', 'TUBING, RESEVOIR TO PUMP', 'Noise', 'https://i.ibb.co/3cYj4YY/1301-00-9390.jpg', 'water leaking'),
+  new GmpFluidsystem('1301-00-9390', 'TUBING, RESEVOIR TO PUMP', 'Noise', 'https://i.ibb.co/3cYj4YY/1301-00-9390.jpg', 'water leaking, F7.5'),
   new GmpFluidsystem('7122-00-9592', 'ASSY, FLOW SWITCH 0.50 GPM, FS Cooling system with Heater', '', 'https://i.ibb.co/KXXBFvK/7122-00-9592.jpg', 'F7.3'),
   new GmpFluidsystem('4508-00-4023', 'CONTRACTOR DP 40A 230VAC COIL', '', 'https://i.ibb.co/5KgLBjq/4508-00-4023.jpg', ''),
-  new GmpFluidsystem('7111-07-2799', 'PCB ASSY ROHS HV SPLIT CAP SPECTRUM', '', 'https://i.ibb.co/GdPsTrh/7111-07-2799.jpg', ''),
+  new GmpFluidsystem('7111-07-2799', 'PCB ASSY ROHS HV SPLIT CAP SPECTRUM', '', 'https://i.ibb.co/GdPsTrh/7111-07-2799.jpg', 'F19.3'),
   new GmpFluidsystem('3450-00-0355', 'PRO SERIES DI CARTRIDGE/ FILTER', '', 'https://i.ibb.co/NTr79XF/3450-00-0355.jpg', ''),
   new GmpFluidsystem('3460-00-0071', 'CAP VENT 2 1/4 BUTTRESS THRD', '', '', ''),
   new GmpFluidsystem('7122-00-9234', 'ASSY FLUID TUBING KIT SPECTRUM', '', '', ''),
@@ -191,40 +188,39 @@ const fluidSystem = [
 // GmpHeadparts 객체 생성
 const headParts = [
   new GmpHeadparts('3160-02-0013', 'Red Manifold O-rings', '', '', ''),
-  new GmpHeadparts('9908-17-0408', "FLASHLMP,REARTIP,700T,4''ARC,8", '', 'https://i.ibb.co/fXBdRH0/9908-17-0408.jpg', 'F5.2'),
-  new GmpHeadparts('7122-00-9578', 'DHP YAG HEAD ASSY', '', 'https://i.ibb.co/bKxnMYv/7122-00-9578.jpg', 'F12.6,F14.2'),
+  new GmpHeadparts('9908-17-0408', "FLASHLMP,REARTIP,700T,4''ARC,8", '', 'https://i.ibb.co/fXBdRH0/9908-17-0408.jpg', 'F5.2, F14.1, F14.2, F12.1, F12.4, F12.3'),
+  new GmpHeadparts('7122-00-9578', 'DHP YAG HEAD ASSY', '', 'https://i.ibb.co/bKxnMYv/7122-00-9578.jpg', 'F12.6, F14.2, F12.1, F19.3, F12.3'),
   new GmpHeadparts('1301-00-9584', 'HEAD MANIFOLD,YAG,DHP', 'Yag Head', 'https://i.ibb.co/cw7D7F9/1301-00-9584.jpg', ''),
   new GmpHeadparts('1301-00-9444', 'HEAD MANIFOLD, SPECTRUM', 'Yag Head', 'https://i.ibb.co/yP9Bm2q/1301-00-9444.jpg', ''),
   new GmpHeadparts('1301-00-9583', 'HEAD MANIFOLD,ALEX,DHP', 'For Alex Head: 2ea', 'https://i.ibb.co/sgppYts/1301-00-9583.jpg', ''),
   new GmpHeadparts('3406-41-0804', 'ELBOW,MALE, 45 DEGREE, 1/2x1/4 NPT,SS', 'Yag Head', 'https://i.ibb.co/bBsG4Cw/3406-41-0804.jpg', ''),
   new GmpHeadparts('3406-10-0804', 'CONN,MALE 1/2T x1/4MPT SS', 'Yag Head', 'https://i.ibb.co/m8zWxVN/3406-10-0804.jpg', ''),
-  new GmpHeadparts('7122-00-9572', 'FSE, GMAX DHP ALEX HEAD', '', '', ''),
+  new GmpHeadparts('7122-00-9572', 'GMAX DHP ALEX HEAD', '', '', 'F14.1, F12.1, F19.3, F12.3'),
   new GmpHeadparts('7122-57-9572', 'FSE, GMAX DHP ALEX HEAD (Service parts: Republished)', 'Service parts: Republished', '', ''),
   new GmpHeadparts('2849-04-0854', 'Head Screw', 'Long Screw', '', ''),
   new GmpHeadparts('3160-02-0013', 'Red Manifold O-rings', '', '', ''),
   new GmpHeadparts('3160-01-0012', 'White O-rings', '', '', ''),
-  new GmpHeadparts('7122-00-9207', 'Pump Cavity', '', '', 'F12,F6.1'),
-  new GmpHeadparts('1301-00-7763', 'Yag ROD', '', '', 'F19'),
-  new GmpHeadparts('1301-00-7014', 'Alex ROD', '', '', 'F19'),
+  new GmpHeadparts('7122-00-9207', 'Pump Cavity', '', '', 'F12, F6.1, F19.3, F12.1, F12.4, F12.3'),
+  new GmpHeadparts('1301-00-7763', 'Yag ROD', '', '', 'F19.3'),
+  new GmpHeadparts('1301-00-7014', 'Alex ROD', '', '', 'F19.3'),
 ];
 
 // GmpHandpieceparts 객체 생성
 const gmphd = [
-  new GmpHandpieceparts('7122-00-9401', 'PRO SERIES DCD HP W/O FIBER', 'GMP Only', 'https://i.ibb.co/F74pWyr/7122-00-9401.jpg', 'Consumable'),
+  new GmpHandpieceparts('7122-00-9401', 'PRO SERIES DCD HP W/O FIBER', 'GMP Only', 'https://i.ibb.co/F74pWyr/7122-00-9401.jpg', 'Consumable, F1.1, F8.3, F13.1, F13.3'),
   new GmpHandpieceparts('7122-00-9404', 'PRO SERIES ACC HP W/O AIR CLIP', 'ACC Handpiece', 'https://i.ibb.co/8r63zvG/7122-00-9404.jpg', ''),
   new GmpHandpieceparts('7122-00-9752', 'ACC LARGE SPOT DELIVERY SYSTEM ASSY', 'ACC Handpiece(Large Spot)', '', ''),
   new GmpHandpieceparts('7122-00-9420', 'PRO SERIES AIR CLIP', 'ACC Handpiece AIR CLIP', 'https://i.ibb.co/kJpwThx/7122-00-9420.jpg', ''),
   new GmpHandpieceparts('7122-00-9527', 'VELCRO STRAP KIT', '', '', ''),
-  new GmpHandpieceparts('7122-00-9585', 'PRO SERIES ACC HP W AIR CLIP W/O FIBER', '', 'https://i.ibb.co/8gJH7XJ/7122-00-9585.jpg', ''),
-  new GmpHandpieceparts('7122-00-9752', 'ACC LARGE SPOT DELIVERY SYSTEM ASSY', '', '', ''),
+  new GmpHandpieceparts('7122-00-9585', 'PRO SERIES ACC HP W AIR CLIP W/O FIBER', '', 'https://i.ibb.co/8gJH7XJ/7122-00-9585.jpg', 'F13.1, F13.3'),
   new GmpHandpieceparts('7122-00-9419', 'PRO SERIES FIBER', '', 'https://i.ibb.co/nz8q336/7122-00-9419.jpg', 'F15.1'),
-  new GmpHandpieceparts('3160-00-1150', 'O-RING, 13/16ID, 3/32W, BUNA-N', 'Inside of Handpiece', 'https://i.ibb.co/dQ5hPWn/3160-00-1150.jpg', ''),
+  new GmpHandpieceparts('3160-00-1150', 'O-RING, 13/16ID, 3/32W, BUNA-N', 'Inside of Handpiece', 'https://i.ibb.co/dQ5hPWn/3160-00-1150.jpg', 'F12.2, F12.1, F12.4, F12.6'),
   new GmpHandpieceparts('1301-00-9457', 'FOAM,PRE-CUT,SPECTRUM FAN', '', '', ''),
-  new GmpHandpieceparts('7122-00-9746', 'LARGE SPOT FIBER ASSY', 'Specialty HP Green Fiber', '', ''),
-  new GmpHandpieceparts('7122-00-9748', 'DCD LARGE SPOT DELIVERY SYSTEM ASSY', 'Large Spot Delivery System\nLarge DCD handpiece\n20/22/24', 'https://i.ibb.co/CsHZSdL/7122-00-9748.jpg', ''),
+  new GmpHandpieceparts('7122-00-9746', 'LARGE SPOT FIBER ASSY', 'Green Fiber', '', 'F15.1'),
+  new GmpHandpieceparts('7122-00-9748', 'DCD LARGE SPOT DELIVERY SYSTEM ASSY', 'Large Spot Delivery System\nLarge DCD handpiece\n20/22/24', 'https://i.ibb.co/CsHZSdL/7122-00-9748.jpg', 'F1.1, F8.3, F13.1, F13.3'),
   new GmpHandpieceparts('7122-00-9602', 'ASSY,SPECIALIZED PRO DCD HP W/O FIBER', 'Specialty HP\n1.5/3/5/3x10\nvascular vein', '', ''),
   new GmpHandpieceparts('7122-00-9616', 'SPECIALTY DELIVERY SYSTEM KIT ASSY', '', '', ''),
-  new GmpHandpieceparts('1303-00-0220', 'FINGER SWITCH, H/P, PRO,ROHS', '', 'https://i.ibb.co/ZXTnV5D/1303-00-0220.jpg', ''),
+  new GmpHandpieceparts('1303-00-0220', 'FINGER SWITCH, H/P, PRO,ROHS', '', 'https://i.ibb.co/ZXTnV5D/1303-00-0220.jpg', 'F13.1, F13.3'),
   new GmpHandpieceparts('1301-00-9401', 'LEFT SHELL DCD H/P SPECTRUM', '', 'https://i.ibb.co/93BqFdp/1301-00-9401.jpg', ''),
   new GmpHandpieceparts('1301-00-9405', 'RIGHT SHELL DCD H/P SPECTRUM', '', 'https://i.ibb.co/Z2DTHWQ/1301-00-9405.jpg', ''),
   new GmpHandpieceparts('1301-00-9403', 'TRIGGER BUTTON H/P SPECTRUM', '', '', '')
@@ -232,8 +228,10 @@ const gmphd = [
 
 // Gmpcontrollerparts 객체 생성
 const gmpController =[
-  new GmpControllerparts('7123-07-0014', 'Split IO', '', 'https://i.ibb.co/XZHsLD0/7123-07-0014.jpg', 'F26.1'),
-  new GmpControllerparts('7123-00-0588', 'ASSY, Q7 LCD DISPLAY', 'New Display for 5digts', 'https://i.ibb.co/PZzs2P8/7123-00-0588.jpg', 'F26.1')
+  new GmpControllerparts('7123-07-0014', 'Split IO', '', 'https://i.ibb.co/XZHsLD0/7123-07-0014.jpg', 'F26.1,Upgrade to Spilt IO, F7.1, F7.2, F18.1, F18.2, F18.3, F8.4, F9.2'),
+  new GmpControllerparts('7123-67-0014', 'Split IO', 'FSE Part', 'https://i.ibb.co/XZHsLD0/7123-07-0014.jpg', 'F26.1, F7.1, F7.2, F18.1, F18.2, F18.3, F8.4'),
+  new GmpControllerparts('7123-00-0588', 'ASSY, Q7 LCD DISPLAY', 'New Display for 5digts', 'https://i.ibb.co/PZzs2P8/7123-00-0588.jpg', 'F26.1,Upgrade to Spilt IO, F24'),
+  new GmpControllerparts('7123-00-0201', 'GUI Display Cable Set Pro Series', 'New Display for 5digts', '', 'Upgrade to Spilt IO')
 ];
 
 // Gmpskinparts 객체 생성
@@ -251,6 +249,22 @@ const gmpskin = [
   new GmpSkinParts('1301-00-9409', 'DCD COVER, SPECTRUM', '', 'https://i.ibb.co/r76ygCx/1301-00-9409.jpg', '')
 ];
 
+// GmpCalportParts 객체 생성
+const gmpcalport = [
+  new GmpCalportParts('8075-00-1015', 'Calport Detector Fiber (Black)', '4 Digits IO', '', 'F15.1'),
+  new GmpCalportParts('8075-00-1019', 'Calport Detector Fiber (Black)', '5 Digits IO', '', 'F15.1, Upgrade to Split IO'),
+  new GmpCalportParts('7122-00-9608', 'ASSY,GMAX PRO CALPORT', '', '', 'F6.5'),
+  new GmpCalportParts('1301-00-9430', 'WINDOW, SPECTRUM CALPORT', 'Calport window', 'https://i.ibb.co/Xkq0k1b/1301-00-9430.jpg', 'F15.1, consumable'),
+  new GmpCalportParts('7122-00-9525', '4 PIN SWITCH ASSY', 'Inside calport', '', 'F6.5'),
+  new GmpCalportParts('7122-00-9524', '5 PIN SWITCH ASSY', 'Inside calport', '', 'F6.5')
+];
+
+// GmpServiceTool 객체 생성
+const gmptool = [
+  new GmpServiceTool('7122-00-3363', 'ASSY,TOOL,ALIGN,CROSSHAIR,MGL', '', 'https://i.ibb.co/FsyjdSt/7122-00-3363.jpg', 'brun plug'),
+  new GmpServiceTool('7122-00-3362', 'ASSY,ALIGN VER TOOL,MGL', '', 'https://i.ibb.co/YhBvzVX/7122-00-3362.jpg', 'fiber burn')
+
+];
 
 // GmppHandpieceparts 객체 생성
 const gmpphd = [
@@ -347,7 +361,7 @@ const NordlysConsumable = [
   new NordlysConsumableParts('4APL7376', 'Circular tip:', 'Nd-Yag HP Tip', '', ''),
   new NordlysConsumableParts('2LAB7543-A01', ' laser warning sign', '', '', ''),
   new NordlysConsumableParts('2SWI7548', 'Key Switch', '', '', ''),
-  new NordlysConsumableParts('9KIT7834', 'Foot Switch', '', '', ''),
+  new NordlysConsumableParts('9KIT7834', 'Foot Switch', '', '', 'F13.2, F13.4'),
   new NordlysConsumableParts('9APH7302', 'Wall-Mount Rack for 4 Ellipse IPL/Nd:YAG Applicators', 'Applicator rack. Hold up to 4 Ellipse IPL/Nd:YAG applicators. Wall mounted', '', ''),
   new NordlysConsumableParts('9APH7839', 'Wall-Mount Rack for 1 Frax + 3 Ellipse IPL/Nd:YAG Applicators', 'Applicator rack. Hold up to 3 Ellipse IPL/Nd:YAG applicators and 1 Frax applicator. Wall mounted', '', ''),
   new NordlysConsumableParts('PU00531EN-NA', 'Nordlys Product Brochure, Ellipse, US', 'Brochure', '', ''),
@@ -397,9 +411,9 @@ app.get('/part/:info', (req, res) => {
 
   let parts;
   if (partType === 'gmp') { // Check the selected part type
-    parts = [...opticsParts, ...consumableParts, ...electronicsParts, ...fluidSystem, ...headParts, ...gmphd, ...gmpController, ...gmpskin];
+    parts = [...opticsParts, ...consumableParts, ...electronicsParts, ...fluidSystem, ...headParts, ...gmphd, ...gmpController, ...gmpskin,  ...gmpcalport, ...gmptool];
   } else if (partType === 'gmpp') {
-    parts = [...gmpphd, ...opticsParts, ...consumableParts, ...electronicsParts, ...fluidSystem, ...headParts];
+    parts = [...gmpphd, ...opticsParts, ...consumableParts, ...electronicsParts, ...fluidSystem, ...headParts,  ...gmpcalport, ...gmptool];
   } else if (partType === 'vbeam') {
     parts = [...vbeamOpticsParts, ...vbeamConsumableParts];
   } else if (partType === 'prima') {
@@ -419,6 +433,8 @@ app.get('/part/:info', (req, res) => {
       ...gmphd,
       ...gmpController,
       ...gmpskin,
+      ...gmpcalport,
+      ...gmptool,
       ...gmpphd,
       ...vbeamOpticsParts,
       ...vbeamConsumableParts,
